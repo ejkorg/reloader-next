b47fdf4c3f638b19542bc78064b67fc4d0f2843c
EJ Korg <186242033+ejkorg@users.noreply.github.com>
2025-01-24 11:19:47 +0000

    add _app.tsx

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+// filepath: /workspaces/reloader-next/src/pages/_app.tsx
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
